48938590cc855c01e56c5cc989cab5503cee2c5e
“preetimca2016” <preetimca2016@gmail.com>
2022-08-11 19:08:46 +0530

    [Preeti] Add: Review Files

---
+++ Review1/ArmstrongNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review1
+{
+    class ArmstrongNumber
+    {
+        public void ArmstrongNum()
+        {
+            Console.WriteLine("Enter the Number :");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int r = 0;
+            int temp = Number;
+            while (Number > 0)
+            {
+                r = Number % 10;
+                sum = sum + (r * r * r);
+                Number = Number / 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine("Number is Armstrong Number");
+            }
+            else
+            {
+                Console.WriteLine("Number is not an Armstrong number");
+            }
+        }
+    }
+}
---
+++ Review1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Review1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArmstrongNumber armstrongNumber = new ArmstrongNumber();
+            armstrongNumber.ArmstrongNum();
+        }
+    }
+}
